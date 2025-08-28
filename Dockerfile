20a8d3ac03783dc30892de3741d192f3b586b732
Ciprian P <ciprian.popa@mountsoftware.ro>
2025-08-28 19:33:41 +0300

    extra port config

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,11 @@ FROM alpine:3.20.3
 ENV FTP_USER=foo \
 	FTP_PASS=bar \
 	GID=1000 \
-	UID=1000
+	UID=1000 \
+	PORT20=20 \
+	PORT21=21 \
+	MIN_PASSIVE_PORT=40000 \
+	MAX_PASSIVE_PORT=40009 
 
 RUN apk add --no-cache --update \
 	vsftpd==3.0.5-r2
@@ -11,5 +15,3 @@ COPY [ "/src/vsftpd.conf", "/etc" ]
 COPY [ "/src/docker-entrypoint.sh", "/" ]
 
 ENTRYPOINT [ "/docker-entrypoint.sh" ]
-EXPOSE 20/tcp 21/tcp 40000-40009/tcp
-HEALTHCHECK CMD netstat -lnt | grep :21 || exit 1
--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -22,4 +22,14 @@ tail -f /var/log/vsftpd.log | tee /dev/stdout &
 touch /var/log/xferlog
 tail -f /var/log/xferlog | tee /dev/stdout &
 
+sed -i "s:listen_port=.*\$:listen_port=$PORT21:" /etc/vsftpd.conf
+sed -i "s:ftp_data_port=.*\$:ftp_data_port=$PORT20:" /etc/vsftpd.conf
+sed -i "s:pasv_min_port=.*\$:pasv_min_port=$MIN_PASSIVE_PORT:" /etc/vsftpd.conf
+sed -i "s:pasv_max_port=.*\$:pasv_max_port=$MAX_PASSIVE_PORT:" /etc/vsftpd.conf
+
+cat /etc/vsftpd.conf
+
+printenv
+
+
 /usr/sbin/vsftpd
